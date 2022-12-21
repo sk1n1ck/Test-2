89d4c4f8ee953f0582a6f1b4fdb6a8a059ac6b3c
sk1n1ck <egorkomarchev@mail.ru>
2022-12-21 21:07:12 +0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="poleno">
+      <p className="poleno1">
+        ALERT!
+      </p>
+      <p className="poleno2">
+      ALERT!
+      </p>
+      <p className="poleno3">
+      ALERT!
+      </p>
+      <p className="poleno4">
+      ALERT!
+      </p>
+      <p className="poleno5">
+      ALERT!
+      </p>
+      <p className="poleno6">
+      ALERT!
+      </p>
+    </div>
+  );
+}
+
+export default App;
